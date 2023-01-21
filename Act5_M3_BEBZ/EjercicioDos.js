b1dd9cc744b133529d000c9417427f225b2e1a71
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-01-21 16:14:25 -0500

    Dos_Ejercicios
    Se sube hasta el ejercicios 2

---
+++ Act5_M3_BEBZ/EjercicioDos.js
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 02
+ * Concatenar con cada elemento del array
+ * 
+ */
+
+let nums = [0,2,4,8]
+
+let i = 0, j = 0;
+let t = nums.length;
+
+for(j; j<t; j++){
+    console.log(nums[i],nums[j]);
+}
---
+++ Act5_M3_BEBZ/EjercicioUno.js
@@ -0,0 +1,15 @@
+/**
+ * 1. **Ejercicio 01**
+    
+    Usando una variable contador y bucle que recorra el array, determinar el tamaño del siguiente array.
+    nums = [7, 4, 0, 9, 7, 6, 3, 8]
+ */
+let nums = [7, 4, 0, 9, 7, 6, 3, 8];
+let cont = 0;
+
+do{
+    console.log(cont, " (", nums[cont], ") ");
+    cont++;
+}while(cont < nums.length);
+
+console.log("El tamaño del vector es de " + cont + " unidades");
