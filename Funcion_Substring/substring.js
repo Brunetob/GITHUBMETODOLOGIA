2d7618181e766dfc9566ade703c59ef42cec016a
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-02-02 19:47:16 -0500

    Update substring.js

---
+++ Funcion_Substring/substring.js
@@ -0,0 +1,14 @@
+/*Estructura del método substring */
+
+"Hola Mundo!".substring(2,6);
+/*Cadena de texto a extraer una parte.substring(parámetro de inicio, parámetro de fin);*/
+
+/*********************************************************************************** */
+
+/*Primer excepción 
+let dato = "Hola cuenca".substring(-1,5);
+console.log(dato);*/
+
+/*Segunda excepción*/
+let string = "Que más ve".substring(4,4);
+console.log(string);
